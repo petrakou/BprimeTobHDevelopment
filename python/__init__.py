b8c2443fb8a5cb71593fdff831320b970b8a2181
petrakou <petrakou@users.noreply.github.com>
2013-10-18 01:59:29 +0300

    Delete __init__.py

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Bprime_kit/BprimeTobHAnalysis/',1)[0])+'/cfipython/slc5_amd64_gcc462/Bprime_kit/BprimeTobHAnalysis')
